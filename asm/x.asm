cf7bd184be0f2c8506009994d52785dda17d2230
ricktjwong <rickwtj@gmail.com>
2018-11-13 17:08:24 +0000

    Change player resolution

--- asm/x.asm
+++ asm/x.asm
@@ -1,103 +1,3 @@
-movlw  0x5
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x19
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x6
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x19
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x7
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x19
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x8
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x19
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x9
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x19
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x12
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x19
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x13
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x19
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x14
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x19
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x15
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x19
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x16
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x19
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
 movlw  0x4
 addwf  player_x, 0
 movwf  LATE
@@ -168,16 +68,6 @@ addwf  player_y, 0
 movwf  LATD
 movlw  0x0A 
 call  add_tiny_delay 
-movlw  0x11
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x18
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
 movlw  0x12
 addwf  player_x, 0
 movwf  LATE
@@ -258,16 +148,6 @@ addwf  player_y, 0
 movwf  LATD
 movlw  0x0A 
 call  add_tiny_delay 
-movlw  0x8
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x17
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
 movlw  0x14
 addwf  player_x, 0
 movwf  LATE
@@ -279,16 +159,6 @@ movwf  LATD
 movlw  0x0A 
 call  add_tiny_delay 
 movlw  0x15
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x17
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x16
 addwf  player_x, 0
 movwf  LATE
 movlw  0x0A 
@@ -318,42 +188,12 @@ addwf  player_y, 0
 movwf  LATD
 movlw  0x0A 
 call  add_tiny_delay 
-movlw  0x13
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x16
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
 movlw  0x14
 addwf  player_x, 0
 movwf  LATE
 movlw  0x0A 
 call  add_tiny_delay 
 movlw  0x16
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x15
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x16
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x7
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x15
 addwf  player_y, 0
 movwf  LATD
 movlw  0x0A 
@@ -378,16 +218,6 @@ addwf  player_y, 0
 movwf  LATD
 movlw  0x0A 
 call  add_tiny_delay 
-movlw  0x12
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x15
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
 movlw  0x13
 addwf  player_x, 0
 movwf  LATE
@@ -398,37 +228,7 @@ addwf  player_y, 0
 movwf  LATD
 movlw  0x0A 
 call  add_tiny_delay 
-movlw  0x14
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x15
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x8
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x14
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
 movlw  0x9
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x14
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0xa
 addwf  player_x, 0
 movwf  LATE
 movlw  0x0A 
@@ -558,57 +358,17 @@ addwf  player_y, 0
 movwf  LATD
 movlw  0x0A 
 call  add_tiny_delay 
-movlw  0xf
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x11
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
 movlw  0x10
 addwf  player_x, 0
 movwf  LATE
 movlw  0x0A 
 call  add_tiny_delay 
 movlw  0x11
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x11
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x11
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0xb
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x10
 addwf  player_y, 0
 movwf  LATD
 movlw  0x0A 
 call  add_tiny_delay 
 movlw  0xc
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x10
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0xd
 addwf  player_x, 0
 movwf  LATE
 movlw  0x0A 
@@ -698,16 +458,6 @@ addwf  player_y, 0
 movwf  LATD
 movlw  0x0A 
 call  add_tiny_delay 
-movlw  0xc
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0xd
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
 movlw  0xd
 addwf  player_x, 0
 movwf  LATE
@@ -758,27 +508,7 @@ addwf  player_y, 0
 movwf  LATD
 movlw  0x0A 
 call  add_tiny_delay 
-movlw  0xe
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0xc
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
 movlw  0xf
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0xc
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x10
 addwf  player_x, 0
 movwf  LATE
 movlw  0x0A 
@@ -828,16 +558,6 @@ addwf  player_y, 0
 movwf  LATD
 movlw  0x0A 
 call  add_tiny_delay 
-movlw  0x11
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0xb
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
 movlw  0xa
 addwf  player_x, 0
 movwf  LATE
@@ -878,27 +598,7 @@ addwf  player_y, 0
 movwf  LATD
 movlw  0x0A 
 call  add_tiny_delay 
-movlw  0x9
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x9
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
 movlw  0xa
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x9
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0xb
 addwf  player_x, 0
 movwf  LATE
 movlw  0x0A 
@@ -924,16 +624,6 @@ movwf  LATE
 movlw  0x0A 
 call  add_tiny_delay 
 movlw  0x9
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x8
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x8
 addwf  player_y, 0
 movwf  LATD
 movlw  0x0A 
@@ -998,27 +688,7 @@ addwf  player_y, 0
 movwf  LATD
 movlw  0x0A 
 call  add_tiny_delay 
-movlw  0x12
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x7
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
 movlw  0x13
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x7
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x14
 addwf  player_x, 0
 movwf  LATE
 movlw  0x0A 
@@ -1108,16 +778,6 @@ addwf  player_y, 0
 movwf  LATD
 movlw  0x0A 
 call  add_tiny_delay 
-movlw  0x4
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x4
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
 movlw  0x5
 addwf  player_x, 0
 movwf  LATE
@@ -1139,56 +799,6 @@ movwf  LATD
 movlw  0x0A 
 call  add_tiny_delay 
 movlw  0x7
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x4
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x8
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x4
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x9
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x4
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0xa
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x4
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x12
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x4
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x13
 addwf  player_x, 0
 movwf  LATE
 movlw  0x0A 
@@ -1219,16 +829,6 @@ movwf  LATD
 movlw  0x0A 
 call  add_tiny_delay 
 movlw  0x16
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x4
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x17
 addwf  player_x, 0
 movwf  LATE
 movlw  0x0A 
@@ -1369,16 +969,6 @@ movwf  LATD
 movlw  0x0A 
 call  add_tiny_delay 
 movlw  0x17
-addwf  player_x, 0
-movwf  LATE
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x3
-addwf  player_y, 0
-movwf  LATD
-movlw  0x0A 
-call  add_tiny_delay 
-movlw  0x18
 addwf  player_x, 0
 movwf  LATE
 movlw  0x0A 
