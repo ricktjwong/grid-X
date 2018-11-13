fc48443161504aa2ab9626f39a240c1c000a926f
ricktjwong <rickwtj@gmail.com>
2018-11-13 17:26:32 +0000

    Add digit 1 for asm

---
+++ asm/1.asm
@@ -0,0 +1,208 @@
+movlw  0xd
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0xa
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0xb
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0xc
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0xd
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0xd
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0xd
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0xd
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0xd
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0xd
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0xd
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0xd
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0xd
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0xd
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0xd
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0xd
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0xd
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0x9
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0xa
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0xb
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0xc
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0xd
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0xe
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0xf
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0x10
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
+movlw  0x11
+addwf  digit_x, 0
+movwf  LATE
+movlw  0x0A 
+call  add_tiny_delay 
+movwf  LATD
+movlw  0x0A 
+call  add_tiny_delay 
